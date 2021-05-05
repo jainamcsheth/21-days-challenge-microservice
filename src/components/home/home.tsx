65f2b0efdec8a449929da0d108728996c26b5a9a
Richa Gupta <rkg3387@gmail.com>
2021-05-05 17:03:15 -0400

    Home component added

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,3 +1,36 @@
 import React from 'react';
+import { Card } from '../card/card';
+import { User } from '../user/user';
+import styles from './home.module.scss';
 
-export const Home: React.FC = () => <div>Home Page</div>;
+export const Home: React.FC = () => (
+  <div className={styles.outerBg}>
+    <div className={styles.innerBg}>
+      <div className={styles.headerRow}>
+        <nav>burger icon</nav>
+        <span className={styles.progress}>32% Done</span>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.leftBar}>
+          <User />
+        </div>
+
+        <div className={styles.mainContent}>
+
+          <section className={styles.row}>
+            <Card />
+            <Card />
+            <Card />
+          </section>
+
+          <section className={styles.row}>
+            <Card />
+            <Card />
+            <Card />
+          </section>
+
+        </div>
+      </div>
+    </div>
+  </div>
+)
