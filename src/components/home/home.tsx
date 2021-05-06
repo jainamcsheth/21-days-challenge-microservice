9e2af6cf90e2fcac6a7dea5bada6edf29f781b1b
Richa Gupta <rkg3387@gmail.com>
2021-05-06 11:28:35 -0400

    home module

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Card } from '../card/card';
+import { Challenge } from '../challenge/challenge';
+import { Day } from '../day/day';
 import { User } from '../user/user';
 import styles from './home.module.scss';
 
 export const Home: React.FC = () => (
   <div className={styles.outerBg}>
     <div className={styles.innerBg}>
+
       <div className={styles.headerRow}>
         <nav>burger icon</nav>
         <span className={styles.progress}>32% Done</span>
       </div>
+
       <div className={styles.row}>
         <div className={styles.leftBar}>
           <User />
@@ -21,12 +25,41 @@ export const Home: React.FC = () => (
             <Card />
             <Card />
             <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </section>
 
+
+
+          <Challenge />
           <section className={styles.row}>
-            <Card />
-            <Card />
-            <Card />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
+            <Day />
           </section>
 
         </div>
