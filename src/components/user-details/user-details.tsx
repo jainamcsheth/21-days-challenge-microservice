8ea819023741160767c1936d6b48f3a1dfd2968b
Richa Gupta <rkg3387@gmail.com>
2021-05-11 18:25:39 -0400

    styling for user details page added

--- src/components/user-details/user-details.tsx
+++ src/components/user-details/user-details.tsx
@@ -5,35 +5,46 @@ import styles from './user-details.module.scss';
 // User details api call
 
 export const UserDetails: React.FC<UserDetailsProps> = ({
- userDetails,userStatistics
+  userDetails,
+  userStatistics,
 }) => {
-
-  const {CompletedChallenges, EnrolledChallenges } = userStatistics;
-  const {Name} = userDetails ;
+  const { Coins, CompletedChallenges, EnrolledChallenges } = userStatistics;
+  const { Name } = userDetails;
   return (
-  <section className={styles.userInfo}>
-    <div className={styles.userIcon}>
-      <img
-        className={styles.userImg}
-        src="https://picsum.photos/100/100"
-        alt="placeholder img"
-      />
-      <h1 className={styles.heading}>
-        Hello
-        {/* <br />   {Name.FirstName}! */}
-        <br/>
+    <section className={styles.userInfo}>
+      <div className={styles.userIcon}>
+        <img
+          className={styles.userImg}
+          src='https://picsum.photos/100/100'
+          alt='placeholder img'
+        />
+        <h1 className={styles.heading}>
+          Hello
+          <br /> {Name?.FirstName}!
+          <br />
+        </h1>
+      </div>
+      <p>
+        Enjoy the little things for one day you may look back and realize they
+        were the big things.
+      </p>
 
-      </h1>
-    </div>
-    <p>
-      Enjoy the little things for one day you may look back and realize they
-      were the big things.
-    </p>
-    You have enrolled to  <strong>{EnrolledChallenges}</strong> challenges
-    <br/>
-    You have completed <strong>{CompletedChallenges}</strong> challenges
+      <h3>Statistics</h3>
+      <div className={styles.statisticsWrapper}>
+        <div className={styles.statisticsBlock}>
+          <h3 className={styles.statisticsValue}>{Coins}</h3>
+          <strong>Total coins</strong>
+        </div>
+        <div className={styles.statisticsBlock}>
+          <h3 className={styles.statisticsValue}>{EnrolledChallenges}</h3>
+          <strong>Current challenges</strong>
+        </div>
 
-  </section>
-);
-  }
-
+        <div className={styles.statisticsBlock}>
+          <h3 className={styles.statisticsValue}>{CompletedChallenges}</h3>
+          <strong>Completed challenges</strong>
+        </div>
+      </div>
+    </section>
+  );
+};
